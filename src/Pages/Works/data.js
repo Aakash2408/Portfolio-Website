9bac421a85f5d930e09a03200b06e17082fd9af4
aakash2408 <aakashsangwan024@gmail.com>
2020-12-07 10:58:04 +0530

    WOrk Data Added

---
+++ src/Pages/Works/data.js
@@ -0,0 +1,74 @@
+export const projects = [
+    {
+        title: 'PathFinding Visualizer',
+        images: [
+            'https://user-images.githubusercontent.com/47134609/101288386-46067480-381c-11eb-9545-628ae069ac0c.png',
+            'https://user-images.githubusercontent.com/47134609/101288383-443cb100-381c-11eb-9ac7-31c3faaf8f4a.png',
+            'https://user-images.githubusercontent.com/47134609/101288386-46067480-381c-11eb-9545-628ae069ac0c.png'
+        ],
+        sourceCodeLink: 'https://github.com/Aakash2408/Covid-19-Stats',
+        demoLink: 'pathfinding-visualizer-31d8e.web.app/',
+        type: 'react'
+    },
+    {
+        title: 'TamatarWala',
+        images: [
+            'https://user-images.githubusercontent.com/47134609/101272889-7287a580-37b6-11eb-8f55-c106090383cc.png',
+            'https://user-images.githubusercontent.com/47134609/101288632-89adae00-381d-11eb-8d68-369f156396df.png',
+            'https://user-images.githubusercontent.com/47134609/101288461-bf9e6280-381c-11eb-9b3c-61dd310986b0.png',
+
+        ],
+        sourceCodeLink: 'https://github.com/Aakash2408/TamatarWala-Online-Veggies-Store',
+        demoLink: 'https://online-shop-32976.firebaseapp.com/',
+        type: 'react'
+    },
+    {
+        title: 'Covid19 Data Tracker',
+        images: [
+            'https://user-images.githubusercontent.com/47134609/101288889-faa19580-381e-11eb-9db4-5de1dd054a5d.png',
+            'https://user-images.githubusercontent.com/47134609/101288796-6f280480-381e-11eb-8c28-eb68e4bb9957.png',
+            'https://user-images.githubusercontent.com/47134609/101288801-73542200-381e-11eb-944b-ee78f8f9abfc.png'
+        ],
+        sourceCodeLink: 'https://github.com/TheCoderDream/Vue-Ecommerce-App-with-Vuex',
+        demoLink: 'http://sincere-wind.surge.sh/',
+        type: 'react'
+    },
+    {
+        title: 'Portfolio-Template ',
+        images: [
+            'https://user-images.githubusercontent.com/47134609/101311755-1f762700-3878-11eb-879a-f2ea22863840.png',
+            'https://user-images.githubusercontent.com/47134609/101311759-20a75400-3878-11eb-8e2f-144d6e89aa88.png',
+            'https://user-images.githubusercontent.com/47134609/101311761-21d88100-3878-11eb-8b39-38dc97677e3e.png'
+        ],
+        sourceCodeLink: 'https://github.com/Aakash2408/Portfolio-Website',
+        demoLink: '/',
+        type: 'react'
+    },
+   
+    {
+        title: 'G.O.T Board Game',
+        images: [
+            'https://user-images.githubusercontent.com/47134609/101312644-49304d80-387a-11eb-972f-d5ca13b89feb.png',
+            'https://user-images.githubusercontent.com/47134609/101312650-4f262e80-387a-11eb-9c30-1ab15b3d8601.png',
+            'https://user-images.githubusercontent.com/47134609/101312655-50eff200-387a-11eb-8998-4b3d92ada3d4.png',
+            'https://user-images.githubusercontent.com/47134609/101312661-53524c00-387a-11eb-9454-3e5360319897.png'
+            
+        ],
+        sourceCodeLink: 'https://github.com/Aakash2408/GAME-OF-THRONES-BOARD-GAME',
+        demoLink: 'https://github.com/Aakash2408/GAME-OF-THRONES-BOARD-GAME',
+        type: 'js'
+    },
+    {
+        title: 'Memory Game',
+        images: [
+            'https://user-images.githubusercontent.com/47134609/101312629-3c135e80-387a-11eb-94c5-d661bd95025f.png',
+            'https://user-images.githubusercontent.com/47134609/101312631-40d81280-387a-11eb-897c-60174a24f9dc.png',
+            'https://user-images.githubusercontent.com/47134609/101312638-433a6c80-387a-11eb-9029-0e653574684e.png',
+            'https://user-images.githubusercontent.com/47134609/101312640-459cc680-387a-11eb-96e7-f55c7463a83a.png'
+        ],
+        sourceCodeLink: 'https://github.com/Aakash2408/Memory-Game',
+        demoLink: 'https://aakash2408.github.io/Memory-Game/',
+        type: 'js'
+    }
+   
+]
---
+++ src/Pages/Works/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { projects } from './data';
+import Title from "../common/Title";
+import WorkItem from "./Item";
+import WorksMenu from "./Menu";
+
+const initalState = projects.filter(p => p.type === 'react');
+
+const Works = () => {
+    const [selectedProjects, setSelectedProjects] = useState(initalState);
+    const [selectedType, setSelectedType] = useState('react');
+
+    function handleMenuChange(type) {
+        setSelectedProjects(projects.filter(p => p.type === type));
+        setSelectedType(type);
+    }
+
+
+    return (
+        <div className="page" id="works">
+            <Title>
+                Projects - Talk is cheap show me the code!
+            </Title>
+            <WorksMenu
+                items={[
+                    'react',
+                    'vue',
+                    'js'
+                ]}
+                selected={selectedType}
+                clicked={handleMenuChange}
+            />
+            <div className="row">
+                {
+                    selectedProjects.map(
+                        item => <div className="column" key={item.sourceCodeLink}>
+                            <WorkItem
+                                {...item}
+                            />
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Works;
